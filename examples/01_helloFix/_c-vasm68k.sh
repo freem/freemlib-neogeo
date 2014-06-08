24cc2f53e89156196231c8966b14fc71565b4364
freem <aj187@ajworld.net>
2014-06-08 18:13:45 -0500

    force vasm to compile for 68000

--- examples/01_helloFix/_c-vasm68k.sh
+++ examples/01_helloFix/_c-vasm68k.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-vasm68k -Fbin -o helloFix_vasm68k.p -devpac 01_helloFix.asm
+vasm68k -Fbin -m68000 -o helloFix_vasm68k.p -devpac 01_helloFix.asm
 byteswap helloFix_vasm68k.p 202-p1.p1
 pad 202-p1.p1 524288 255
